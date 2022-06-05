f53ed1b4cbe671992f41b8c3c412ff98cf78ed8a
Hani J <hanijandali@gmail.com>
2022-06-05 13:12:22 -0700

    Additional progress

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const str = 'hello world this is me'
 
-// Challenge 1 
-const arr = str.split('')
+// CHALLENGE 1 
+
+/* const arr = str.split('')
 arr[0] = arr[0].toUpperCase()
 console.log( arr.join('-') )
+*/
 
 function capitalize(string) {
     const first = string[0].toUpperCase()
@@ -13,5 +15,23 @@ function capitalize(string) {
 
 console.log( capitalize(str) )
 
-// Challenge 2
+// CHALLENGE 2 - Function allCaps
 
+function allCaps(str) {
+    return str.toUpperCase()
+}
+
+console.log(allCaps(str))
+
+// CHALLENGE 3 - capitalizeWords makes the first character of each word uppercase, each word separated by a string
+
+function capitalizeWords(str) {
+    const array = str.split(' ')
+    // split between each space, i.e. each word
+    const upperWords = array.map( word => capitalize(word) )
+    return upperWords.join(' ')
+}
+
+console.log(capitalizeWords(str))
+console.log(capitalizeWords('Where did everyone go'))
+// Tests show successful capitalization of words
