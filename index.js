a35b56d9b844ffa70ead520eb41059719e3dd02d
Hani J <hanijandali@gmail.com>
2022-06-05 23:45:37 -0700

    Completion of camel case and shift functions

--- index.js
+++ index.js
@@ -2,11 +2,6 @@ const str = 'hello world this is me'
 
 // CHALLENGE 1 
 
-/* const arr = str.split('')
-arr[0] = arr[0].toUpperCase()
-console.log( arr.join('-') )
-*/
-
 function capitalize(string) {
     const first = string[0].toUpperCase()
     const rest = string.slice(1)
@@ -50,3 +45,71 @@ function extraSpacesErase(str) {
 const example_spaces = '    Hello        World!     '
 
 console.log( extraSpacesErase(example_spaces) )
+
+// Challenge 5 - Filter out unwanted characters
+
+function preciseFilter(str, separator = '-'){
+    const lower = str.toLowerCase()
+    const chars = lower.split('')
+    const filtered = chars.filter((char) => {
+        const code = char.charCodeAt(0)
+        if (code > 96 && code < 123) {
+            return true
+        } else if (code > 47 && code < 58) {
+            return true
+        } else if (code == 32 || code == 45) {
+            return true
+        }
+        return false
+    })
+    const freeSpace = extraSpacesErase(filtered.join(''))
+    return freeSpace.split(' ').join(separator)
+}
+    
+const example_filter_char = '    Hello World!!!   hypo-alo-genic    1, 2, 3, 4, 99     '
+
+console.log(preciseFilter(example_filter_char))
+
+// Challenge 6 - create snakeCase() that removes extra space and replaces spaces with underscore and makes all characters lowercase
+// Will be a modification on above preciseFilter function
+
+function snakeCase(str) {
+    return preciseFilter(str, '_')
+}
+console.log(snakeCase(example_filter_char))
+
+// Challenge 7 - Lowercase the first character of the first word and uppercase the first character of all other words
+// while removing all spaces
+// Example: Camel Case -> camelCase
+
+function camelCase(str) {
+    // Capitalize all words. If we have a long string, all words will have first letter capitalized
+    const words = capitalizeWords(str)
+    // trim off white spaces
+    const trimmed = words.trim()
+    // Split the words into an array
+    const camel_array = trimmed.split(' ')
+    // Lowercase the first index of our array
+    const first_lower = camel_array[0].toLowerCase()
+    // Join with split array of all capitalized words and return
+    const remainder = camel_array.slice(1)
+    const all_words = remainder.join('')
+    return first_lower + all_words
+    
+}
+
+camel_example = "camel case censor here"
+
+console.log(camelCase(camel_example))
+
+// Challenge 8 - shift() takes the first character of a string and moves to the end of a string
+// Example: Hello World -> ello WorldH
+
+function shift(str) {
+    firstChar = str[0]
+    string = str.slice(1)
+    return string + firstChar
+}
+
+shift_example = "Hello World"
+console.log(shift(shift_example))
